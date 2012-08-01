aa112e01a6bb397412b20038ec74143975ed91f6
root <root@ubuntu.(none)>
2012-08-01 10:04:12 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a4a9435fbb0081585e2060b90843b7fd9edf6839edd4a64d0f6bcf8c35e612747a94b75eac04f06ac2dde06c452c1a74ce616ccb675a4233be9a27294a3dbd19'
